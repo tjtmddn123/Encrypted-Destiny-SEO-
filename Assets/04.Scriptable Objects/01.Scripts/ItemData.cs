4412cfb6a71336e6a1902fbfae3a2b4a94ad388e
LaBear2 <op9339291@gmail.com>
2024-01-12 09:59:12 +0900

    [Meta] 옵젝수정

---
+++ Assets/04.Scriptable Objects/01.Scripts/ItemData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+}
+
+[CreateAssetMenu]
+
+
+public class ItemData : ScriptableObject
+{
+    
+}
