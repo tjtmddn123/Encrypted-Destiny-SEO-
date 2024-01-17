929fdb94a3b968c4a112e26a21ff17ab2a38d480
colarrot <gunho7935@gmail.com>
2024-01-17 19:34:14 +0900

    LightAndText

--- Assets/02.Scripts/05.UI/StartScene/Paper.cs
+++ Assets/02.Scripts/05.UI/StartScene/Paper.cs
@@ -1,18 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Paper : MonoBehaviour
+public class PaperInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject heldPaper; 
+    public bool isHoldingPaper = false; 
+
+    public void Update()
     {
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            if (!isHoldingPaper)
+            {
+                PickUpPaper();
+            }
+            else
+            {
+                
+                DropPaper();
+            }
+        }
+
         
+        if (isHoldingPaper)
+        {
+            MoveHeldPaper();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PickUpPaper()
     {
         
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            if (hit.collider.CompareTag("Paper"))
+            {
+                isHoldingPaper = true;
+                heldPaper = hit.collider.gameObject;
+            }
+        }
+    }
+
+    public void DropPaper()
+    {
+        
+        isHoldingPaper = false;
+        heldPaper = null;
+    }
+
+    public void MoveHeldPaper()
+    {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            heldPaper.transform.position = new Vector3(hit.point.x, hit.point.y, heldPaper.transform.position.z);
+        }
     }
 }
