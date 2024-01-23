25460c92e907c85ef688aed45655b9564363b4ae
LaBear2 <op9339291@gmail.com>
2024-01-23 17:48:43 +0900

    [Other] 벽 텍스트 추가

--- Assets/02.Scripts/03.Items/nightvisionitemtest.cs
+++ Assets/02.Scripts/03.Items/nightvisionitemtest.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class nightvisionitemtest : MonoBehaviour
 {
     public GameObject WallText;
+    public GameObject RText;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
-            WallText.SetActive(!WallText.activeInHierarchy); 
+            WallText.SetActive(!WallText.activeInHierarchy);
+            RText.SetActive(!RText.activeInHierarchy);
         }
     }
 }
