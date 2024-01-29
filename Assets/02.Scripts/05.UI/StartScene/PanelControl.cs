99347f1c2084550cd5742a00475f4e704d0430c8
colarrot <gunho7935@gmail.com>
2024-01-29 19:16:07 +0900

    0129

--- Assets/02.Scripts/05.UI/StartScene/PanelControl.cs
+++ Assets/02.Scripts/05.UI/StartScene/PanelControl.cs
@@ -9,6 +9,44 @@ public class MenuOption : MonoBehaviour
     public GameObject menu;
     public GameObject option;
 
+    private bool isPaused = false;
+
+
+    private void Start()
+    {
+        option.SetActive(false); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {       
+        Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true; 
+        option.SetActive(true); 
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {      
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false; 
+        option.SetActive(false); 
+        isPaused = false;
+    }
 
     public void ActivatePanel1()
     {
@@ -16,10 +54,13 @@ public class MenuOption : MonoBehaviour
         option.SetActive(false);
     }
 
-
+    
     public void ActivatePanel2()
     {
         menu.SetActive(false);
         option.SetActive(true);
     }
+
+   
+    
 }
