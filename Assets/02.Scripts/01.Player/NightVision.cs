1309e68575dcfbe2e90f3713b7d2311804a78d0b
tjtmddn123 <149347374+tjtmddn123@users.noreply.github.com>
2024-02-22 19:28:40 +0900

    [Fix] 라이트 빛퍼짐 효과 완화
    씬정리 및 라이트 빛 퍼짐 효과 완화, 관련 스크립트 수정!

--- Assets/02.Scripts/01.Player/NightVision.cs
+++ Assets/02.Scripts/01.Player/NightVision.cs
@@ -1,24 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NightVision : MonoBehaviour
 {
-    public GameObject nightvision;
-    public bool hasNightVision = false;
+    public GameObject nightvision; // 나이트 비전 기능을 가진 게임 오브젝트
+    public bool hasNightVision = false; // 나이트 비전 보유 여부
+    public List<Light> LightObj; // 라이트 컴포넌트를 가진 오브젝트 목록
 
+    // 나이트 비전 아이템을 플레이어에게 추가
     public void AddNightVisionItem()
     {
         hasNightVision = true;
     }
 
+    // 매 프레임마다 사용자 입력을 체크하여 나이트 비전 활성화 여부를 토글
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.N))
+        if (Input.GetKeyDown(KeyCode.N) && hasNightVision)
         {
-            if (hasNightVision)
+            // 나이트 비전 활성화 상태를 토글
+            nightvision.SetActive(!nightvision.activeInHierarchy);
+            // 라이트의 Render Mode를 토글 상태에 따라 설정
+            ToggleLights(nightvision.activeInHierarchy);
+        }
+    }
+
+    // 라이트 목록의 Render Mode를 설정
+    private void ToggleLights(bool isNightVisionActive)
+    {
+        foreach (Light light in LightObj)
+        {
+            // 나이트 비전이 활성화되었을 경우 Render Mode를 Auto로 설정
+            if (isNightVisionActive)
             {
-                nightvision.SetActive(!nightvision.activeInHierarchy);
+                light.renderMode = LightRenderMode.Auto;
+            }
+            // 나이트 비전이 비활성화되었을 경우 Render Mode를 Important = ForcePixel로 설정
+            else
+            {
+                light.renderMode = LightRenderMode.ForcePixel;
             }
         }
     }
--- Assets/02.Scripts/05.UI/SW_KeypadEventUI.cs
+++ Assets/02.Scripts/05.UI/SW_KeypadEventUI.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
-
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class SW_KeypadEventUI : MonoBehaviour
 {
